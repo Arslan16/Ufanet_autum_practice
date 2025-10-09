45b3e992c13ada92828f7f8b0617a028f8f89458
Arslan16 <arslanshaidulin@yandex.ru>
2025-10-09 14:25:30 +0500

    +test_get_card_info_by_card_id +test_get_all_rows_from_table

--- tests/factories.py
+++ tests/factories.py
@@ -1,40 +1,47 @@
-from hypothesis import strategies as st, HealthCheck
-from string import ascii_letters, digits
+import uuid
+
+from hypothesis import HealthCheck
+from hypothesis import strategies as st
+
+my_hypothesis_settings = {
+    "max_examples": 3,
+    "derandomize": False,
+    "deadline": None,
+    "suppress_health_check": [HealthCheck.function_scoped_fixture, HealthCheck.too_slow]
+}
 
 
-my_hypothesis_settings = dict(
-    max_examples=3, 
-    derandomize=False,
-    deadline=None, 
-    suppress_health_check=[HealthCheck.function_scoped_fixture, HealthCheck.too_slow]
-)
+@st.composite
+def card_factory(draw):
+    return {
+        "main_label": str(uuid.uuid4())[:10],
+        "description_under_label": str(uuid.uuid4())[:10],
+        "obtain_method_description": str(uuid.uuid4())[:10],
+        "validity_period": str(uuid.uuid4())[:10],
+        "about_partner": str(uuid.uuid4())[:10],
+        "promocode": str(uuid.uuid4())[:10],
+        "call_to_action_link": str(uuid.uuid4())[:10],
+        "call_to_action_btn_label": str(uuid.uuid4())[:10]
+    }
 
-SAFE_CHARS = ascii_letters + digits
 
-safe_text = lambda min_size, max_size: st.text(alphabet=SAFE_CHARS, min_size=min_size, max_size=max_size)
-
-card_factory = st.fixed_dictionaries({
-    "id": st.integers(min_value=100, max_value=10000),
-    "main_label": safe_text(3, 20),
-    "description_under_label": safe_text(3, 50),
-    "obtain_method_description": safe_text(3, 50),
-})
-
-queue_factory = safe_text(5, 15)
+@st.composite
+def queue_factory(draw):
+    name = str(uuid.uuid4())[:5]
+    suffix = str(uuid.uuid4())[:10] 
+    return f"{name}_{suffix}"
 
 
 @st.composite
 def company_factory(draw):
-    name = draw(safe_text(5, 10))
-    suffix = draw(st.integers(1, 10000))
-    short_description = draw(safe_text(10, 50))
+    name = str(uuid.uuid4())[:5]
+    suffix = str(uuid.uuid4())[:10]
+    short_description = str(uuid.uuid4())[:15] 
     return {"name": f"{name}_{suffix}", "short_description": short_description}
 
 
 @st.composite
 def category_factory(draw):
-    name = draw(safe_text(5, 10))
-    suffix = draw(st.integers(1, 10000))
+    name = str(uuid.uuid4())[:5]
+    suffix = str(uuid.uuid4())[:10] 
     return {"name": f"{name}_{suffix}"}
-
-
--- tests/test_database_utils.py
+++ tests/test_database_utils.py
@@ -1,45 +1,30 @@
-import pytest
 from unittest.mock import AsyncMock, patch
+
+import pytest
+from factories import card_factory, category_factory, company_factory, my_hypothesis_settings, queue_factory
 from hypothesis import given, settings
-from sqlalchemy import select, delete, desc, insert
-from sqlalchemy.ext.asyncio import (
-    AsyncSession, 
-    create_async_engine, 
-    async_sessionmaker,
-    AsyncEngine
-)
+from hypothesis import strategies as st
+from sqlalchemy import delete, desc, select
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
 
 from core.core_types import OutBoxStatuses
-from core.models import (
-    BaseTable, 
-    CategoriesTable,
-    CompaniesTable, 
-    OutboxTable,
-    CardsTable
-)
-
 from core.database_utils import (
     create_row,
     get_all_cards_in_category_with_short_description,
-    insert_into_outbox,
     get_all_categories,
+    get_all_rows_from_table,
+    get_card_info_by_card_id,
     get_last_pending_messages_from_outbox,
-    set_status_of_outbox_row
+    insert_into_outbox,
+    set_status_of_outbox_row,
 )
-
-from factories import (
-    my_hypothesis_settings,
-    card_factory,
-    category_factory,
-    company_factory,
-    queue_factory
-)
+from core.models import BaseTable, CardsTable, CategoriesTable, CompaniesTable, OutboxTable
 
 
 @pytest.fixture(scope="function")
 async def session():
     engine: AsyncEngine = create_async_engine(
-        f"postgresql+asyncpg://postgres:postgres@localhost/ufanet_test_db",
+        "postgresql+asyncpg://postgres:postgres@localhost/ufanet_test_db",
         echo=False,
     )
     async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
@@ -69,15 +54,15 @@ async def cleanup_session(session: AsyncSession):
 
 @pytest.mark.asyncio
 @given(
-    queue_name=queue_factory,
-    payload=card_factory
+    queue_name=queue_factory(),
+    payload=card_factory()
 )
 @settings(**my_hypothesis_settings)
 async def test_insert_into_outbox(
     session: AsyncSession,
     queue_name: str,
     payload: dict
-):   
+):
     await insert_into_outbox(
         payload=payload,
         queue=queue_name,
@@ -93,8 +78,8 @@ async def test_insert_into_outbox(
 
 @pytest.mark.asyncio
 @given(
-    queue_name=queue_factory, 
-    payload=card_factory
+    queue_name=queue_factory(),
+    payload=card_factory()
 )
 @settings(**my_hypothesis_settings)
 async def test_get_last_pending_messages_from_outbox(
@@ -108,26 +93,26 @@ async def test_get_last_pending_messages_from_outbox(
         session=session,
         commit=True
     )
-    
+
     result = await get_last_pending_messages_from_outbox(session)
     assert isinstance(result, list)
     assert len(result) >= 1
     assert result[-1].payload == payload
     assert result[-1].queue == queue_name
-    
+
     await set_status_of_outbox_row(result[-1].id, OutBoxStatuses.FAILED, session)
     await session.refresh(result[-1])
     assert result[-1].status == OutBoxStatuses.FAILED
-    
-    
+
+
 @pytest.mark.asyncio
 @given(
-    queue_name=queue_factory, 
+    queue_name=queue_factory(),
     category=category_factory()
 )
 @settings(**my_hypothesis_settings)
 async def test_get_all_categories(
-    session: AsyncSession, 
+    session: AsyncSession,
     queue_name: str,
     category: dict
 ):
@@ -137,7 +122,7 @@ async def test_get_all_categories(
         await create_row(
             CategoriesTable,
             category,
-            session, 
+            session,
             queue_name
         )
         await session.commit()
@@ -152,8 +137,8 @@ async def test_get_all_categories(
 @given(
     category=category_factory(),
     company=company_factory(),
-    card=card_factory,
-    queue_name=queue_factory,
+    card=card_factory(),
+    queue_name=queue_factory()
 )
 @settings(**my_hypothesis_settings)
 async def test_get_all_cards_in_category_with_short_description(
@@ -178,15 +163,84 @@ async def test_get_all_cards_in_category_with_short_description(
         assert result[-1]["main_label"] == card["main_label"]
 
 
-# @pytest.mark.asyncio
-# async def test_get_card_info_by_card_id(session: AsyncSession):
-#     ...
+@pytest.mark.asyncio
+@given(
+    card=card_factory(),
+    category=category_factory(),
+    company=company_factory(),
+    queue_name=queue_factory()
+)
+@settings(**my_hypothesis_settings)
+async def test_get_card_info_by_card_id(
+    session: AsyncSession,
+    card: dict,
+    category: dict,
+    company: dict,
+    queue_name: str
+):
+    with patch("core.database_utils.insert_into_outbox", new_callable=AsyncMock):
+        category_id = await create_row(CategoriesTable, category, session, queue_name)
+        company_id = await create_row(CompaniesTable, company, session, queue_name)
+        card["category_id"] = category_id
+        card["company_id"] = company_id
+        card_id = await create_row(CardsTable, card, session, queue_name)
+        assert isinstance(card_id, int)
+        my_card = await get_card_info_by_card_id(card_id, session, queue_name)
+        for key in my_card:
+            assert card[key] == my_card[key]
 
 
-# @pytest.mark.asyncio
-# async def test_get_all_rows_from_table(session: AsyncSession):
-#     ...
+@pytest.mark.asyncio
+@given(
+    categories=st.lists(category_factory(), min_size=5, max_size=10,  unique_by=lambda x: x["name"]),
+    companies=st.lists(company_factory(), min_size=5, max_size=10,  unique_by=lambda x: x["name"]),
+    cards=st.lists(card_factory(), min_size=5, max_size=10, unique_by=lambda x: x["main_label"]),
+    queue_name=queue_factory()
+)
+@settings(**my_hypothesis_settings)
+async def test_get_all_rows_from_table(
+    session: AsyncSession,
+    categories: list[dict],
+    companies: list[dict],
+    cards: list[dict],
+    queue_name: str
+):
+    with patch("core.database_utils.insert_into_outbox", new_callable=AsyncMock):
+        names: list = []
+        for category in categories:
+            category["id"] = await create_row(CategoriesTable, category, session, queue_name)
+            names.append(category["name"])
 
+        db_categories = await get_all_rows_from_table(CategoriesTable, session, queue_name)
+        db_names = {c["name"] for c in db_categories}
+        expected_names = {c["name"] for c in categories}
+
+        assert expected_names <= db_names
+
+        names: list = []
+        for company in companies:
+            company["id"] = await create_row(CompaniesTable, company, session, queue_name)
+            names.append(company["name"])
+
+        db_companies = await get_all_rows_from_table(CompaniesTable, session, queue_name)
+        db_names = {c["name"] for c in db_companies}
+        expected_names = {c["name"] for c in companies}
+
+        assert expected_names <= db_names
+
+
+        names: list = []
+        for card in cards:
+            card["category_id"] = categories[0]["id"]
+            card["company_id"] = companies[0]["id"]
+            card["id"] = await create_row(CardsTable, card, session, queue_name)
+            names.append(card["main_label"])
+
+        db_cards = await get_all_rows_from_table(CardsTable, session, queue_name)
+        db_names = {c["main_label"] for c in db_cards}
+        expected_names = {c["main_label"] for c in cards}
+
+        assert expected_names <= db_names
 
 # @pytest.mark.asyncio
 # async def test_get_full_row_for_admin_by_id(session: AsyncSession):
